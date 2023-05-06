51fc386d4618992d7aee8040835e17e9a50dcdd1
nick thomas <nichoth@gmail.com>
2023-05-06 15:41:06 -0700

    create profile & test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-export function example ():void {
-    console.log('hello')
+import timestamp from 'monotonic-timestamp'
+import { Crypto } from '@oddjs/odd'
+import { writeKeyToDid } from '@ssc-hermes/util'
+import { create as createMsg, SignedRequest } from '@ssc-hermes/message'
+
+export interface Profile {
+    humanName: string
+    author: string
+    username: string
+    rootDID: string
+    description?: string
+    timestamp:number
+}
+
+interface ProfileArgs {
+    humanName:string,
+    username:string,
+    description?:string,
+    rootDID?:string
+}
+
+/**
+ * Create a signed Profile object in-memory. Root DID can be passed in, or if not we
+ * assume that it is the DID being used to sign the message.
+ * @param crypto {Crypto.Implementation}
+ * @param args {{humanName, username, description?, rootDID?}}
+ * @returns {Promise<SignedRequest<Profile>>} The new profile + signature
+ */
+export async function create (crypto:Crypto.Implementation, args:ProfileArgs)
+:Promise<SignedRequest<Profile>> {
+    return createMsg(crypto, Object.assign({}, args, {
+        // author comes from `createMsg`
+        timestamp: timestamp(),
+        rootDID: (args.rootDID || await writeKeyToDid(crypto))
+    }))
 }
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,35 @@
 import { test } from 'tapzero'
+import * as odd from '@oddjs/odd'
+import { verify } from '@ssc-hermes/message'
+import { components } from '@ssc-hermes/node-components'
+import * as profile from '@ssc-hermes/profile'
+import { writeKeyToDid } from '@ssc-hermes/util'
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
+let program
+
+test('setup', async t => {
+    program = await odd.assemble({
+        namespace: { creator: 'test', name: 'testing' },
+        debug: false
+    }, components)
+
+    t.ok(program, 'create a program')
+})
+
+test('profile.create', async t => {
+    const { crypto } = program.components
+    const newProfile = await profile.create(crypto, {
+        humanName: 'Alice',
+        username: 'alice-123'
+    })
+
+    t.ok(newProfile, 'should return a new profile')
+    t.equal(newProfile.humanName, 'Alice', 'should have the right human name')
+    const did = await writeKeyToDid(crypto)
+
+    t.equal(newProfile.author, did, 'should have .author')
+    t.equal(newProfile.rootDID, did, 'should have a `rootDID` property')
+
+    const isValid = await verify(newProfile)
+    t.equal(isValid, true, 'the new profile should have a valid signature')
 })
